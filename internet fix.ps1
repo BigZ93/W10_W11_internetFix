67705a9870ad9df26c90ec6ad00892a489b6df54
Michał <39052982+BigZ93@users.noreply.github.com>
2023-05-03 12:54:11 +0200

    Add files via upload

---
+++ internet fix.ps1
@@ -0,0 +1,24 @@
+﻿#ipconfig /all
+#netsh interface show interface
+
+ipconfig /relese
+Start-Sleep -s 1
+ipconfig /flushdns
+Start-Sleep -s 1
+ipconfig /renew
+Start-Sleep -s 1
+netsh int ip set dns
+Start-Sleep -s 1
+netsh int ip reset
+Start-Sleep -s 1
+netsh winsock reset
+Start-Sleep -s 1
+$t = 10
+while($t -gt 0){
+    echo ("Komputer zrestartuje sie za " + $t + " s")
+    $t--
+    Start-Sleep -s 1
+}
+Restart-Computer
+
+#panel sterowania -> siec i wspoldzielenie -> ustawienia adaptera -> wlasciwosci -> ipv4 -> wlasciwosci -> automatyczny dns lub 8.8.8.8 i 8.8.4.4
